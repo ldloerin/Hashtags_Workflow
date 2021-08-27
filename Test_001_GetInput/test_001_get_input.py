3b94a9143f786ca2dd2991f4578c585eac9ac89a
David Loerinci <david.loerinci@gmail.com>
2021-08-27 09:38:34 +0200

    cleaning

--- Test_001_GetInput/test_001_get_input.py
+++ Test_001_GetInput/test_001_get_input.py
@@ -1,5 +1,5 @@
 import os
-# import pytest
+import pytest
 import sys
 sys.path.append(os.path.split(os.path.dirname(__file__))[0])
 from TestServices.Config.get_test_input import GetTestInput
@@ -11,11 +11,9 @@ input = my_code.execute_workflow()
 
 
 class TestClass:
-    def test_001_general_hashtag_file(self):
-        assert config.general_hashtag_file == input.general_hashtag_file
-
-    def test_002_essential_hashtag_file(self):
-        assert config.essential_hashtag_file == input.essential_hashtag_file
-
-    def test_002_output_hashtag_file(self):
-        assert config.output_hashtag_file == input.output_hashtag_file
+    @pytest.mark.parametrize("test_value, reference_value", 
+                            [(config.general_hashtag_file, input.general_hashtag_file), 
+                            (config.essential_hashtag_file, input.essential_hashtag_file), 
+                            (config.output_hashtag_file, input.output_hashtag_file)])
+    def test_001_general_hashtag_file(self, test_value, reference_value):
+        assert test_value == reference_value
