90a9bd0d63bcdafbc348d36f2fadc39602f3651b
David Loerinci <david.loerinci@gmail.com>
2021-08-27 12:16:30 +0200

    update

--- TestServices/Config/get_test_input.py
+++ TestServices/Config/get_test_input.py
@@ -1,6 +1,5 @@
 import os
 from TestServices.Config.read_test_json_file import ReadTestJsonFile
-from TestServices.Config.build_paths import BuildPaths
 
 
 class GetTestInput:
@@ -8,6 +7,13 @@ class GetTestInput:
         self.code_path = os.path.dirname(main_code_file)
         self.root_path = os.path.dirname(self.code_path)
         self.__get_input_parameters()
+        try:
+            self.__get_file_path('main_source_code')
+            self.__get_file_path('general_hashtag_file')
+            self.__get_file_path('essential_hashtag_file')
+            self.__get_file_path('output_hashtag_file')
+        except Exception as ex:
+            print(ex)
 
     def __get_input_parameters(self):
         parameter_file = os.path.join(self.code_path, "Input", "config.json")
@@ -15,7 +21,9 @@ class GetTestInput:
         inputs = ReadTestJsonFile.load(parameter_file)
         for key, value in inputs.items():
             setattr(self, key, value)
-        try:
-            BuildPaths(self)
-        except:
-            pass
+
+    def __get_file_path(self, key):
+        relative_path = getattr(self, key)
+        file_path = os.path.join(self.root_path, relative_path)
+        file_path = file_path.replace('\\', '/')
+        setattr(self, key, file_path)
