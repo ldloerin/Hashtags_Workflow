6ea9d68b021f83555a5686d74016447ebfac53da
David Loerinci <david.loerinci@gmail.com>
2021-08-27 09:49:49 +0200

    cleaning

--- Test_001_GetInput/test_001_get_input.py
+++ Test_001_GetInput/test_001_get_input.py
@@ -11,11 +11,10 @@ input = my_code.execute_workflow()
 
 
 class TestClass:
-    @pytest.mark.parametrize(
-        "test_value, reference_value",
-        [(config.general_hashtag_file, input.general_hashtag_file),
-        (config.essential_hashtag_file, input.essential_hashtag_file),
-        (config.output_hashtag_file, input.output_hashtag_file)]
-        )
+    @pytest.mark.parametrize("test_value, reference_value",
+                            [(config.general_hashtag_file, input.general_hashtag_file),
+                            (config.essential_hashtag_file, input.essential_hashtag_file),
+                            (config.output_hashtag_file, input.output_hashtag_file)]
+                            )
     def test_001_general_hashtag_file(self, test_value, reference_value):
         assert test_value == reference_value
